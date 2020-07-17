246c72cb1ac0cc83b253a3b08f9db846daf67909
vuonga1103 <vuonga1103@yahoo.com>
2020-07-16 23:57:31 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Song.destroy_all
+Artist.destroy_all
+Genre.destroy_all
+
+alicia = Artist.create(name: 
+"Alicia Keys", bio: "Hi, I am Alicia")
+
+hip_hop = Genre.create(name: "hip hop")
+
+empire = Song.create(name: "Empire State of Mind", artist_id: alicia.id, genre_id: hip_hop.id)
