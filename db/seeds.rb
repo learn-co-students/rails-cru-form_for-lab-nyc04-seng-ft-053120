9d591d80cd896b73a420bf4c63378384518326ee
kelseyjdickson <kelseymedved@gmail.com>
2020-07-20 12:34:06 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "starting seeding!"
+
+artists = Artist.create([
+    {
+        name: "Whitney Houston",
+        bio: "The best female artist to have ever lived!"
+    },
+    {
+        name: "NSYNC",
+        bio: "Former boy band"
+    },
+    {
+        name: "Trevor Dickson",
+        bio: "Newest, hottest voice for 2020!"
+    }
+    ])
+
+    genres = Genre.create([
+        {
+            name: 'Pop'
+        },
+        {
+            name: 'Boy band'
+        },
+        {
+            name: 'Singer-Songwriter'
+        }
+    ])
+
+
+
+
+
+
+
+songs = Song.create([
+    {
+        name:"I'm every woman",
+        artist_id: Artist.last.id ,
+        genre_id: Genre.last.id
+    },
+    {
+        name:"It's going to May",
+        artist_id: Artist.last.id ,
+        genre_id: Genre.last.id
+    },
+    {
+        name:"Old man and a Pigeon in Love",
+        artist_id: Artist.last.id ,
+        genre_id: Genre.last.id
+    }
+])
+
+
+
+
+puts "Seeding complete!"
